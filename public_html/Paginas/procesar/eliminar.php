a61dc9710e0d552dc25fb66f2972e1debcfe2c56
jesusguerrero21 <lobo_x_black@hotmail.com>
2014-11-22 18:28:14 -0600

    Actuliacion de Consultas  en la tabla de usuarios para la Interfaz de Admin, Rosio,Ana Iris, Jesus Guerrero

---
+++ public_html/Paginas/procesar/eliminar.php
@@ -0,0 +1,38 @@
+<center>
+<?php 
+require_once 'config.php';
+
+	
+	$id=$_GET['IdCliente'];
+	
+	$query="DELETE FROM usuarios WHERE IdCliente='$IdCliente'";
+	
+	$resultado=$mysqli->query($query);
+	
+?>
+
+<html>
+	<head>
+		<title>Eliminar usuario</title>
+	</head>
+	
+	<body>
+		<center>
+			<?php 
+				if($resultado>0){
+				?>
+				
+				<h1>Usuario Eliminado</h1>
+				
+				<?php 	}else{ ?>
+				
+				<h1>Error al Eliminar Usuario</h1>
+				
+			<?php	} ?>
+			<p></p>		
+			
+			<a href="index.php">Regresar</a>
+			
+		</center>
+	</body>
+</html>
