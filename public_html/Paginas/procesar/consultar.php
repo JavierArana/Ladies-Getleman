25eee723a806d4729d6c223205fdcca875f679f5
Jesusguerrero21 <lobo_x_black@hotmail.com>
2014-11-24 00:58:18 -0600

    parte de consultas restantes para administrador Rosio,Ana Iris, Jesus Guerrero

--- public_html/Paginas/procesar/consultar.php
+++ public_html/Paginas/procesar/consultar.php
@@ -21,12 +21,158 @@ echo "<tr>
 		<td>$fila[7]</td>
 		<td>$fila[8]</td>
         <td>
-		<a href='eliminar.php'><img class='imagen' src='../img/usuario_Borrar.png'></a>
+		<img class='imagen' src='../img/usuario_Borrar.png'  name='Elim' onclick='eliminar()'>
 		<img class='imagen' src='../img/usuario_editar.png'>
 		</td>	
 	</tr>";
 
+
 }
+
+
+
+mysqli_close($conexion);
+mysqli_free_result($consulta);
+}
+
+
+#----------------------------------------------consulta de productos------------------------------------------
+public function consultarProductos(){
+$conexion =  mysqli_connect(config::$servidor, config::$usuario, config::$password, config::$baseDeDatos);
+$consulta = mysqli_query($conexion, "select * from productos");
+
+while($fila = mysqli_fetch_array($consulta)){
+echo "<tr>
+		<td>$fila[0]</td>
+		<td>$fila[1]</td>
+		<td>$fila[2]</td>
+		<td>$fila[3]</td>
+		<td>$fila[4]</td>
+		<td>$fila[5]</td>
+		<td>$fila[6]</td>
+		<td>$fila[7]</td>
+		
+		
+        <td>
+		<img class='imagen' src='../img/usuario_Borrar.png'  name='Elim' onclick='eliminar()'>
+		<img class='imagen' src='../img/usuario_editar.png'>
+		</td>	
+	</tr>";
+
+
+}
+
+
+
+mysqli_close($conexion);
+mysqli_free_result($consulta);
+}
+
+#------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+#----------------------------------------------consulta de proveedores-----------------------
+public function consultarProveedores(){
+$conexion =  mysqli_connect(config::$servidor, config::$usuario, config::$password, config::$baseDeDatos);
+$consulta = mysqli_query($conexion, "select * from proveedores");
+
+while($fila = mysqli_fetch_array($consulta)){
+echo "<tr>
+		<td>$fila[0]</td>
+		<td>$fila[1]</td>
+		<td>$fila[2]</td>
+		<td>$fila[3]</td>
+		<td>$fila[4]</td>
+		<td>$fila[5]</td>
+		<td>$fila[6]</td>
+	
+		
+        <td>
+		<img class='imagen' src='../img/usuario_Borrar.png'  name='Elim' onclick='eliminar()'>
+		<img class='imagen' src='../img/usuario_editar.png'>
+		</td>	
+	</tr>";
+
+
+}
+
+
+
+mysqli_close($conexion);
+mysqli_free_result($consulta);
+}
+
+
+#----------------------------------------------------------------------------------------
+
+
+#------------------------consulta de sugerencias--------------------------
+
+public function consultarSugerencias(){
+$conexion =  mysqli_connect(config::$servidor, config::$usuario, config::$password, config::$baseDeDatos);
+$consulta = mysqli_query($conexion, "select * from sugerencias");
+
+while($fila = mysqli_fetch_array($consulta)){
+echo "<tr>
+		<td>$fila[0]</td>
+		<td>$fila[1]</td>
+		<td>$fila[2]</td>
+		<td>$fila[3]</td>
+		
+	
+		
+        <td>
+		<img class='imagen' src='../img/usuario_Borrar.png'  name='Elim' onclick='eliminar()'>
+		<img class='imagen' src='../img/usuario_editar.png'>
+		</td>	
+	</tr>";
+
+
+}
+
+
+
+mysqli_close($conexion);
+mysqli_free_result($consulta);
+}
+
+
+
+
+#------------------------------------------------------------------------
+
+#------------------------------------consulta de ventas----------------------
+public function consultarVentas(){
+$conexion =  mysqli_connect(config::$servidor, config::$usuario, config::$password, config::$baseDeDatos);
+$consulta = mysqli_query($conexion, "select * from ventas");
+
+while($fila = mysqli_fetch_array($consulta)){
+echo "<tr>
+		<td>$fila[0]</td>
+		<td>$fila[1]</td>
+		<td>$fila[2]</td>
+		<td>$fila[3]</td>
+		<td>$fila[4]</td>
+		<td>$fila[5]</td>
+		<td>$fila[6]</td>
+		
+	
+		
+        <td>
+		<img class='imagen' src='../img/usuario_Borrar.png'  name='Elim' onclick='eliminar()'>
+		<img class='imagen' src='../img/usuario_editar.png'>
+		</td>	
+	</tr>";
+
+
+}
+
+
 
 mysqli_close($conexion);
 mysqli_free_result($consulta);
@@ -49,8 +195,19 @@ mysqli_free_result($consulta);
 
 return $res;
 }
-
+function eliminar(){
+	$IdCliente=$_GET['IdCliente'];
+	
+	$query="DELETE FROM usuarios WHERE IdCliente='$IdCliente'";
+	
+	$resultado=$mysqli->query($query);
 }
 
+#---------------------------------------------------------------------
+
+
+
+
+}
 ?>
 </center>
